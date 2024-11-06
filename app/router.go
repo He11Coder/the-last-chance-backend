61ef3f0dd08cb4c93084e4ed811a9841e1006241
Vladimir Borozenets <borozenets2002@mail.ru>
2024-11-06 19:34:11 +0300

    Add pet profiles

--- app/router.go
+++ app/router.go
@@ -13,11 +13,11 @@ import (
 )
 
 type PetInfo struct {
-	TypeOfAnimal string   `json:"type_of_animal"`
-	Breed        string   `json:"breed"`
-	Name         string   `json:"name"`
-	Allergens    []string `json:"allergens"`
-	Preferences  []string `json:"preferences"`
+	PetID        int    `json:"pet_id,omitempty"`
+	TypeOfAnimal string `json:"type_of_animal"`
+	Name         string `json:"name"`
+	Info         string `json:"info"`
+	PetAvatar    string `json:"avatar"`
 }
 
 type LoginCredentials struct {
@@ -26,34 +26,101 @@ type LoginCredentials struct {
 }
 
 type UserInfo struct {
-	UserID    int    `json:"user_id"`
+	UserID    int    `json:"user_id,omitempty"`
 	Username  string `json:"username"`
 	Contacts  string `json:"contacts"`
 	UserImage string `json:"user_image"`
+}
+
+type PetIDList struct {
+	PetIDs []int `json:"pet_ids"`
 }
 
 type ErrorToSend struct {
 	Message string `json:"message"`
 }
 
+var PetDB = map[int]PetInfo{
+	1: {
+		TypeOfAnimal: "Кошка",
+		Name:         "Мурка",
+		Info:         "Очень ласковая кошка. Имеет аллергию на мышей.",
+		PetAvatar:    "cat1.png",
+	},
+	2: {
+		TypeOfAnimal: "Кошка",
+		Name:         "Маркиза",
+		Info:         "Дико злая кошка. Имеет аллергию на людей.",
+		PetAvatar:    "cat2.png",
+	},
+	3: {
+		TypeOfAnimal: "Кот",
+		Name:         "Гладиатор",
+		Info:         "Бешеный кот. Ласков когда спит. Плохо пахнет",
+		PetAvatar:    "cat3.png",
+	},
+	4: {
+		TypeOfAnimal: "Змея",
+		Name:         "Елизавета",
+		Info:         "Почти не кусает. Кусает очень редко, но всегда с летальным исходом. Быть осторожным. Но вообще она очень милая и ласковая.",
+		PetAvatar:    "snake.png",
+	},
+	5: {
+		TypeOfAnimal: "Черепаха",
+		Name:         "Танк",
+		Info:         "Любит молоко...",
+		PetAvatar:    "turtle.png",
+	},
+	6: {
+		TypeOfAnimal: "Собака",
+		Name:         "Кузя",
+		Info:         "Собака с развитым любопытством.",
+		PetAvatar:    "dog1.png",
+	},
+	7: {
+		TypeOfAnimal: "Собака",
+		Name:         "Лизка",
+		Info:         "У собаки аллергия на других собак. Любит бегать.",
+		PetAvatar:    "dog2.png",
+	},
+}
+
 func GetPetInfo(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	_, convErr := strconv.Atoi(vars["petID"])
+	petID, convErr := strconv.Atoi(vars["petID"])
 	if convErr != nil {
 		errToSend := ErrorToSend{Message: "incorrect pet ID"}
 		jsonErr, _ := json.Marshal(errToSend)
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusBadRequest)
 		w.Write(jsonErr)
+		return
 	}
 
-	petInfo := PetInfo{
-		TypeOfAnimal: "Собака",
-		Breed:        "Корги",
-		Name:         "Мага",
-		Allergens:    []string{"Конфеты", "Молоко"},
-		Preferences:  []string{"Кости", "Мясо"},
+	petInfo, ok := PetDB[petID]
+	if !ok {
+		errToSend := ErrorToSend{Message: "pet ID not found"}
+		jsonErr, _ := json.Marshal(errToSend)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusNotFound)
+		w.Write(jsonErr)
+		return
 	}
+
+	CURR_DIR, _ := os.Getwd()
+
+	fileBytes, err := os.ReadFile(CURR_DIR + "/assets/pets" + "/" + petInfo.PetAvatar)
+	if err != nil {
+		errToSend := ErrorToSend{Message: "error while reading pet's avatar"}
+		jsonErr, _ := json.Marshal(errToSend)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write(jsonErr)
+		return
+	}
+
+	base64Image := base64.StdEncoding.EncodeToString(fileBytes)
+	petInfo.PetAvatar = base64Image
 
 	jsonInfo, _ := json.Marshal(petInfo)
 	w.Header().Set("Content-Type", "application/json")
@@ -71,6 +138,7 @@ func Login(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusBadRequest)
 		w.Write(jsonErr)
+		return
 	}
 
 	var loginInfo LoginCredentials
@@ -81,6 +149,7 @@ func Login(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusBadRequest)
 		w.Write(jsonErr)
+		return
 	}
 
 	//Check the credentials in database. Authorization.
@@ -101,6 +170,7 @@ func GetUserInfo(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusBadRequest)
 		w.Write(jsonErr)
+		return
 	}
 
 	CURR_DIR, _ := os.Getwd()
@@ -112,6 +182,7 @@ func GetUserInfo(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write(jsonErr)
+		return
 	}
 
 	base64Image := base64.StdEncoding.EncodeToString(fileBytes)
@@ -137,6 +208,7 @@ func GetUserAvatar(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusBadRequest)
 		w.Write(jsonErr)
+		return
 	}
 
 	CURR_DIR, _ := os.Getwd()
@@ -148,11 +220,33 @@ func GetUserAvatar(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write(jsonErr)
+		return
 	}
 
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/octet-stream")
 	w.Write(fileBytes)
+}
+
+func GetUsersPets(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	_, convErr := strconv.Atoi(vars["userID"])
+	if convErr != nil {
+		errToSend := ErrorToSend{Message: "incorrect user ID"}
+		jsonErr, _ := json.Marshal(errToSend)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(jsonErr)
+		return
+	}
+
+	petIDs := []int{1, 2, 3, 4, 5, 6, 7}
+	petList := PetIDList{PetIDs: petIDs}
+
+	jsonPetList, _ := json.Marshal(petList)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonPetList)
 }
 
 func Run() error {
@@ -162,6 +256,7 @@ func Run() error {
 	router.HandleFunc("/login", Login).Methods("POST")
 	router.HandleFunc("/get_user_info/{userID}", GetUserInfo).Methods("GET")
 	router.HandleFunc("/get_avatar/{userID}", GetUserAvatar).Methods("GET")
+	router.HandleFunc("/get_pet_list/{userID}", GetUsersPets).Methods("GET")
 
 	http.Handle("/", router)
 
